5edb1fc7ff4e1185d1e8b9602728ef804cda1ebe
proshunsuke <shunsuke0901@gmail.com>
2015-12-09 19:45:41 +0900

    10.2.4 パスワード再設定する

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -40,6 +40,11 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
   # メールアドレスをすべて小文字にする
